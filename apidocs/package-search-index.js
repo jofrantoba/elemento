1aa86d3ac9fe9a79dc60d92359eea0a8081e161d
Harald Pehl <harald.pehl@gmail.com>
2020-01-17 19:55:07 +0100

    Cleanup

--- apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.jboss.gwt.elemento.core"},{"l":"org.jboss.gwt.elemento.core.builder"},{"l":"org.jboss.gwt.elemento.processor"},{"l":"org.jboss.gwt.elemento.processor.context"},{"l":"org.jboss.gwt.elemento.template"}]
